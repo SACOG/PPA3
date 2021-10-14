130faeb630fb4844c7d12686250afe2db569ff83
djconly85 <dconly@sacog.org>
2021-10-14 07:44:10 -0700

    working version of complete streets batch calculator that calcualtes complete streets score for all segments in a network.

---
+++ cs-region-batch/README.md
@@ -0,0 +1,5 @@
+# Complete Streets Regional Batch Tool
+
+## Purpose
+
+This tool calculates the PPA Complete Streets Index (CSI) for all road segments provided in a shapefile or feature class of road segments (e.g., SACOG's regional centerline file). With a CSI value for all road segments, PPA3 will be able to normalize each project's CSI on a 0-1 or 0-100 scale based on how the project's CSI compares to the highest CSI in the region or in the project's community type.
---
+++ cs-region-batch/cs_batch_calc.py
@@ -0,0 +1,146 @@
+# --------------------------------
+# Name: complete_street_score.py
+# Purpose: Calculate complete street index (CSI) for project, which is proxy 
+#       to describe how beneficial complete streets treatments would be for the project segment.
+#
+# Author: Darren Conly
+# Last Updated: <date>
+# Updated by: <name>
+# Copyright:   (c) SACOG
+# Python Version: 3.x
+# --------------------------------
+import datetime as dt
+import os
+
+import arcpy
+arcpy.env.overwriteOutput = True
+
+import csi_params as params
+import landuse_buff_calcs as lubuff
+import transit_svc_measure as ts
+
+    
+def complete_streets_idx(fc_pclpt, fc_project, project_type, posted_speedlim, transit_event_fc):
+    '''Calculate complete street index (CSI) for project
+        CSI = (students/acre + daily transit vehicle stops/acre + BY jobs/acre + BY du/acre) * (1-(posted speed limit - threshold speed limit)*speed penalty factor)
+        '''
+    # col_area_ac
+    # col_k12_enr
+    # col_emptot
+    # col_du
+    # cs_buffdist
+    # cs_threshold_speed
+    # cs_spd_pen_fac
+    
+    
+    # don't give complete street score for freeway projects or if sponsor didn't enter speed limit
+    if project_type == params.ptype_fwy or posted_speedlim <= 1: 
+        csi = -1
+    else:
+        # arcpy.AddMessage("Calculating complete street score...")
+    
+        # get transit service density around project
+        tran_stops_dict = ts.transit_svc_density(fc_project, transit_event_fc, project_type)
+        transit_svc_density = list(tran_stops_dict.values())[0]
+    
+        lu_fac_cols = [params.col_area_ac, params.col_k12_enr, params.col_emptot, params.col_du]
+        lu_vals_cols = [params.col_k12_enr, params.col_emptot, params.col_du]
+    
+        # get sums of the lu_fac_cols within project buffer area
+        # import pdb; pdb.set_trace()
+        lu_vals_obj = lubuff.LandUseBuffCalcs(fc_pclpt, fc_project, project_type, lu_fac_cols, params.cs_buffdist)
+        lu_vals_dict = lu_vals_obj.point_sum()
+        # print(lu_vals_dict)
+    
+        #dens_score = (student_dens + trn_svc_dens + job_dens + du_dens)
+        if lu_vals_dict[params.col_area_ac] == 0:
+            dens_score = 0
+        else:
+            dens_score = sum([lu_vals_dict[i] / lu_vals_dict[params.col_area_ac] for i in lu_vals_cols]) + transit_svc_density
+    
+        csi = dens_score * (1 - (posted_speedlim - params.cs_threshold_speed) * params.cs_spd_pen_fac)
+
+    out_dict = {'complete_street_score': csi}
+    
+    return out_dict
+
+def make_fc_with_csi(network_fc, transit_event_fc, fc_pclpt, project_type):
+    start_time = dt.datetime.now()
+    
+    fld_oid = "OBJECTID"
+    fld_geom = "SHAPE@"
+    fld_strtname = "FULLSTREET"
+    fld_spd = "SPEED"
+    fld_len = "SHAPE@LENGTH"
+    fld_csi = "CompltStreetIdx"
+    
+    fields_network = [fld_geom, fld_strtname, fld_spd, fld_len, fld_oid]
+    
+    time_sufx = str(dt.datetime.now().strftime('%m%d%Y%H%M'))
+    output_fc = "CompleteStreetMap{}".format(time_sufx)
+    
+    arcpy.CreateFeatureclass_management(arcpy.env.workspace, output_fc,"POLYLINE", spatial_reference = 2226)
+    
+    arcpy.AddField_management(output_fc, fld_strtname, "TEXT")
+    arcpy.AddField_management(output_fc, fld_spd, "SHORT")
+    arcpy.AddField_management(output_fc, fld_csi, "FLOAT")
+    
+    
+    fl_network = "fl_network"
+    if arcpy.Exists(fl_network): arcpy.Delete_management(fl_network)
+    arcpy.MakeFeatureLayer_management(network_fc, fl_network)
+
+    total_net_links = arcpy.GetCount_management(fl_network)[0]
+    
+    print("inserting rows...")
+    with arcpy.da.InsertCursor(output_fc, [fld_geom, fld_strtname, fld_spd, fld_csi]) as inscur:
+        with arcpy.da.SearchCursor(fl_network, fields_network) as cur:
+            for i, row in enumerate(cur):
+                if i % 1000 == 0:
+                    print(f"{i} out of {total_net_links} rows processed")
+                geom = row[0]
+                stname = row[1]
+                speedlim = row[2]
+                # seglen = row[3]
+                oid = row[4]
+                
+                sql = "{} = {}".format(fld_oid, oid) # BIG PROBLEM = THE LAND USE BUFF CALCS TAKES IN A FEATURE CLASS, NOT A FEATURE SO THIS NEEDS TO BE FIXED
+                arcpy.SelectLayerByAttribute_management(fl_network, "NEW_SELECTION", sql)
+                fc_featurecnt = arcpy.GetCount_management(fl_network)[0]
+                # print("{} features in input network FC are selected".format(fc_featurecnt))
+
+                temp_fc_segment = "TEMP_segment"
+                temp_fc_segment_path = os.path.join(arcpy.env.scratchGDB, temp_fc_segment)
+                # import pdb; pdb.set_trace()
+                arcpy.FeatureClassToFeatureClass_conversion(fl_network, arcpy.env.scratchGDB, temp_fc_segment)
+                
+                csi_dict = complete_streets_idx(in_pcl_pt_fc, temp_fc_segment_path, project_type, speedlim, transit_event_fc)
+                csi = csi_dict['complete_street_score']
+                
+                ins_row = [geom, stname, speedlim, csi]
+                inscur.insertRow(ins_row)
+    time_elapsed = dt.datetime.now() - start_time
+    print("Finished! Processed {} rows in {}".format(i, time_elapsed))
+
+    output_path = os.path.join(arcpy.env.workspace, output_fc)
+    print(f"Output feature class in {output_path}")
+        
+
+
+if __name__ == '__main__':
+    # import pdb; pdb.set_trace()
+    arcpy.env.workspace = r"C:\\PPA_CS_batch_temp\\TEMP_PPA_cs_data.gdb"
+
+    # input fc of parcel data--must be points!
+    in_pcl_pt_fc = os.path.join(params.fgdb, params.parcel_pt_fc_yr(in_year=2016))
+    value_fields = [params.col_area_ac, params.col_k12_enr, params.col_emptot, params.col_du]
+    ptype = 'Arterial'
+
+    # input line project for basing spatial selection
+    # NOTE - input files should come from local drive in case network connections fail
+    input_network_fc = 'Centerline_ArterialCollector10132021'
+    trnstops_fc = os.path.join(params.fgdb, params.trn_svc_fc)
+
+    make_fc_with_csi(input_network_fc, trnstops_fc, in_pcl_pt_fc, ptype)
+    
+
