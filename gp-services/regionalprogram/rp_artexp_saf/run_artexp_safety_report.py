0956b3274b6d373c45fb20cb9b6b68e746b4ea3f
djconly85 <dconly@sacog.org>
2022-03-12 12:56:34 -0800

    working version of art exp safety report script running

--- gp-services/regionalprogram/rp_artexp_saf/run_artexp_safety_report.py
+++ gp-services/regionalprogram/rp_artexp_saf/run_artexp_safety_report.py
@@ -38,7 +38,7 @@ def update_tbl_multiple_geos(json_obj, proj_level_val, k_chartname_metric, metri
     val_ctyp = ixn_aggdict[metric_outdictkey][proj_commtype]
     val_regn = ixn_aggdict[metric_outdictkey][params.geo_region]
 
-    json_obj[k_chartname_metric][params.geo_proj_qmi] = proj_level_val
+    json_obj[k_chartname_metric][params.geo_project] = proj_level_val
     json_obj[k_chartname_metric][params.geo_ctype] = val_ctyp
     json_obj[k_chartname_metric][params.geo_region] = val_regn
 
@@ -46,8 +46,6 @@ def update_tbl_multiple_geos(json_obj, proj_level_val, k_chartname_metric, metri
 def make_safety_report_artexp(fc_project, project_name, project_type, proj_aadt):
     
     in_json = os.path.join(params.json_templates_dir, "SACOG_{Regional Program}_{Arterial_or_Transit_Expasion}_Safety_sample_dataSource.json")
-    lu_buffdist_ft = params.ilut_sum_buffdist # land use buffer distance
-    data_years = [2016, 2040]
 
     with open(in_json, "r") as j_in: # load applicable json template
         loaded_json = json.load(j_in)
@@ -73,20 +71,64 @@ def make_safety_report_artexp(fc_project, project_name, project_type, proj_aadt)
 
     
     # make collision heat map
-
+    img_obj_colln = imgmaker.MakeMapImage(fc_project, "CollisionHeat", project_name)
+    colln_img_path = img_obj_colln.exportMap()
+    loaded_json["Collision heat map Image Url"] = colln_img_path
     
     # calculate collisions per 100MVMT within all geos
+    kv_coll_rate = "FATAL_COLLISNS_PER_100MVMT"
+    k_tblname_collrate = "Collisions per 100 million VMT"
+    colln_rate_proj = collision_data_project[kv_coll_rate]
+    update_tbl_multiple_geos(json_obj=loaded_json, proj_level_val=colln_rate_proj,
+                            k_chartname_metric=k_tblname_collrate, metric_outdictkey=kv_coll_rate,
+                            proj_commtype=project_commtype)
 
 
     # Bike ped collision rate per cline mile within all geos
+    kv_coll_bkpd = "BIKEPED_COLLISNS_PER_CLMILE"
+    k_tblname_collbkpd = "Bike and Ped Collisions per Project Centerline"
+    colln_bkpd_proj = collision_data_project[kv_coll_bkpd]
+    update_tbl_multiple_geos(json_obj=loaded_json, proj_level_val=colln_bkpd_proj,
+                            k_chartname_metric=k_tblname_collbkpd, metric_outdictkey=kv_coll_bkpd,
+                            proj_commtype=project_commtype)
 
 
     # chart of bike/ped and fatal collisions within all geos
+    type_lkp_dict = {"PCT_FATAL_COLLISNS": "Pct of collisions that are fatal",
+                    "PCT_BIKEPED_COLLISNS": "Pct of collisions with bike/ped"}
+
+    typekeys = list(type_lkp_dict.keys())
+
+    for i, k in enumerate(typekeys):
+        k_chartname_bpfatal = "Fatal and BikePed Collisions"
+        typeval = type_lkp_dict[k]
+        val_proj = collision_data_project[k]
+        aggdict = aggvals.make_aggval_dict(aggval_csv=params.aggval_csv, metric_cols=[k], 
+                                                    proj_ctype=project_commtype, yearkey=params.k_year, 
+                                                    geo_regn=params.geo_region, yearval=None)
+
+        val_ctyp = aggdict[k][project_commtype]
+        val_regn = aggdict[k][params.geo_region]
+        
+
+        loaded_json[params.k_charts][k_chartname_bpfatal][params.k_features][i][params.k_attrs] \
+            [params.k_type] = typeval
+        loaded_json[params.k_charts][k_chartname_bpfatal][params.k_features][i][params.k_attrs] \
+            [params.geo_project] = val_proj
+        loaded_json[params.k_charts][k_chartname_bpfatal][params.k_features][i][params.k_attrs] \
+            [params.geo_ctype] = val_ctyp
+        loaded_json[params.k_charts][k_chartname_bpfatal][params.k_features][i][params.k_attrs] \
+            [params.geo_region] = val_regn
+
+        
+
+
+
 
 
     # write out to new JSON file
     output_sufx = str(dt.datetime.now().strftime('%Y%m%d_%H%M'))
-    out_file_name = f"MultiModalRpt{project_name}{output_sufx}.json"
+    out_file_name = f"SafetyRpt{project_name}{output_sufx}.json"
 
     out_file = os.path.join(output_dir, out_file_name)
     
@@ -107,8 +149,8 @@ if __name__ == '__main__':
     project_aadt = arcpy.GetParameterAsText(2) 
 
     # hard values for testing
-    # project_fc = r'I:\Projects\Darren\PPA3_GIS\PPA3Testing.gdb\TestJefferson'
-    # project_name = 'TestJefferson'
+    # project_fc = r'I:\Projects\Darren\PPA_V2_GIS\PPA_V2.gdb\PPAClientRun_SacCity_StocktonBl'
+    # project_name = 'TestStockton'
     # project_aadt = 27000
 
     ptype = params.ptype_arterial
