2d3b059919fb2c30f8e6cc945c1ddc602bded9e3
djconly85 <dconly@sacog.org>
2022-03-21 16:14:57 -0700

    successful test of toolbox with art sgr econ script

---
+++ gp-services/regionalprogram/rp_artsgr_econ/chart_accessibility.py
@@ -0,0 +1,67 @@
+"""
+Name: chart_accessibility.py
+Purpose: update JSON file for charts that show grouped bar chart of accessibility by
+    mode, for project, community type, and region
+
+
+Author: Darren Conly
+Last Updated: Feb 2022
+Updated by: 
+Copyright:   (c) SACOG
+Python Version: 3.x
+"""
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__))) # enable importing from parent folder
+
+import pandas as pd
+
+import parameters as params
+import accessibility_calcs as acc_calcs
+import get_agg_values as aggvs
+
+
+def update_json(json_loaded, fc_project, project_type, project_commtype, aggval_csv, 
+                destination_type, k_chart_title):
+
+    fc_accessibility_data = params.accdata_fc
+
+    # project level dict of accessibility script outputs
+    dict_data = acc_calcs.get_acc_data(fc_project, fc_accessibility_data, project_type) 
+
+    # lookup dict between names of data points in raw output and names in JSON file
+    acc_metrics = {f'WALKDESTS{destination_type}':'30 Min Walk', f'BIKEDESTS{destination_type}':'30 Min Biking', 
+                    f'AUTODESTS{destination_type}':'15 Min Drive', f'TRANDESTS{destination_type}':'45 Min Transit'}
+    accmetrics_keys = list(acc_metrics.keys())
+
+    # trimmed down output, only containing the accessibility metrics needed for this chart 
+    # instead of all accessibility metrics
+    dict_data2 = {k:dict_data[k] for k in acc_metrics.keys()}
+
+    # make dict of regional and comm type values
+    aggval_dict = aggvs.make_aggval_dict(aggval_csv, metric_cols=accmetrics_keys, proj_ctype=project_commtype, 
+                    yearkey=params.k_year, geo_regn=params.geo_region)
+
+    for i, k in enumerate(list(acc_metrics.keys())):
+        
+        # update value for "type" (mode of tranpsortation)
+        json_loaded[params.k_charts][k_chart_title][params.k_features][i] \
+            [params.k_attrs][params.k_type] = acc_metrics[k]
+        
+        # update value for mode's access from project
+        json_loaded[params.k_charts][k_chart_title][params.k_features][i] \
+            [params.k_attrs][params.geo_project] = dict_data2[k] 
+        
+        # update value for mode's access avg for project comm type
+        json_loaded[params.k_charts][k_chart_title][params.k_features][i] \
+            [params.k_attrs][params.geo_ctype] = aggval_dict[k][project_commtype] 
+        
+        # update value for mode's access avg for region
+        json_loaded[params.k_charts][k_chart_title][params.k_features][i] \
+            [params.k_attrs][params.geo_region] = aggval_dict[k][params.geo_region] 
+
+    print("calculated accessibility values sucessfully")
+
+
+if __name__ == '__main__':
+    pass
---
+++ gp-services/regionalprogram/rp_artsgr_econ/chart_lu_acre_change.py
@@ -0,0 +1,33 @@
+"""
+Name: job_du_tot.py
+Purpose: Make chart comparing base vs. future year quantity of specified land use type
+
+
+Author: Darren Conly
+Last Updated: Feb 2022
+Updated by: 
+Copyright:   (c) SACOG
+Python Version: 3.x
+"""
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__))) # enable importing from parent folder
+
+import get_lutype_acres as get_acres
+import parameters as params
+
+def update_json(json_loaded, data_year, order_val, fc_poly_parcels, project_fc, project_type,
+                in_lu_type, k_chart_title):
+    
+    lu_rpt_obj = get_acres.GetLandUseArea(project_fc, project_type, fc_poly_parcels)
+    lu_rpt_dict_year = lu_rpt_obj.get_lu_acres(in_lu_type)
+
+    # update applicable field values in JSON template
+    data_key = f'net_{in_lu_type}_acres'
+    lutype_acres = lu_rpt_dict_year[data_key]
+    json_loaded[params.k_charts][k_chart_title][params.k_features][order_val][params.k_attrs][params.k_year] = str(data_year) # JSON chart requires year label to be string
+    json_loaded[params.k_charts][k_chart_title][params.k_features][order_val][params.k_attrs][params.k_value] = lutype_acres
+
+
+if __name__ == '__main__':
+    pass
