c1b3d79009ff59900516cc2014712082249057dc
djconly85 <dconly@sacog.org>
2022-10-04 20:23:57 -0700

    started work on fwyexp_vmt update

--- gp-services/regionalprogram/rp_artexp_eq/run_equity_report.py
+++ gp-services/regionalprogram/rp_artexp_eq/run_equity_report.py
@@ -156,15 +156,15 @@ if __name__ == '__main__':
     email = arcpy.GetParameterAsText(8)
 
     # hard-coded vals for testing
-    project_fc = r'\\data-svr\GIS\Projects\Darren\PPA3_GIS\PPA3Testing.gdb\TestBroadway16th' # Broadway16th_2226
-    project_name = 'broadway'
-    jurisdiction = 'sac city'
-    project_type = params.ptype_arterial
-    perf_outcomes = 'TEST;Reduce Congestion;Reduce VMT'
-    aadt = 150000
-    posted_spd = 65
-    pci = 80
-    email = 'fake@test.com'
+    # project_fc = r'\\data-svr\GIS\Projects\Darren\PPA3_GIS\PPA3Testing.gdb\TestBroadway16th' # Broadway16th_2226
+    # project_name = 'broadway'
+    # jurisdiction = 'sac city'
+    # project_type = params.ptype_arterial
+    # perf_outcomes = 'TEST;Reduce Congestion;Reduce VMT'
+    # aadt = 150000
+    # posted_spd = 65
+    # pci = 80
+    # email = 'fake@test.com'
 
     uis = params.user_inputs
     input_parameter_dict = {
--- gp-services/regionalprogram/rp_fwyexp_vmt/run_vmt_report.py
+++ gp-services/regionalprogram/rp_fwyexp_vmt/run_vmt_report.py
@@ -18,15 +18,21 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__))) # enable importing f
 import datetime as dt
 from time import perf_counter as perf
 import json
-import pandas as pd
 import arcpy
-
+arcpy.SetLogHistory(False) # prevents an XML log file from being created every time script is run; long terms saves hard drive space
 
 import parameters as params
 import link_occup_data
+import utils.utils as utils
 
 
-def make_vmt_report_fwyexp(fc_project, project_name, project_type):
+# def make_vmt_report_fwyexp(fc_project, project_name, project_type):
+def make_vmt_report_fwyexp(input_dict):
+
+    uis = params.user_inputs
+    fc_project = input_dict[uis.geom]
+    project_name = input_dict[uis.name]
+    project_type = input_dict[uis.ptype] 
     
     in_json = os.path.join(params.json_templates_dir, "SACOG_{Regional Program}_{Freeway}_ReduceVMT_sample_dataSource.json")
     data_years = [2016, 2040]
@@ -35,10 +41,13 @@ def make_vmt_report_fwyexp(fc_project, project_name, project_type):
         loaded_json = json.load(j_in)
 
     # calculate weekday transit trips and avg vehicle occupancy
+    output_data = {}
     for i, year in enumerate(sorted(data_years)):
         fc_links_yr = params.model_links_fc(year)
         out_dict = link_occup_data.get_linkoccup_data(fc_project=fc_project, 
                                 project_type=project_type,fc_model_links=fc_links_yr)
+
+        # what the out_dict looks like: {'avg_2way_trantrips': 447.89406870675947, 'avg_2way_vehocc': 1.348670013997033}
 
         proj_trantrips = out_dict["avg_2way_trantrips"]
         cname_trantrp = "Weekday Transit Trips"
@@ -59,7 +68,22 @@ def make_vmt_report_fwyexp(fc_project, project_name, project_type):
         loaded_json[params.k_charts][cname_vehocc][params.k_features][i] \
             [params.k_attrs][params.k_value] =  proj_vehocc       
 
+        import pdb; pdb.set_trace()
 
+    # log to data table
+    project_uid = utils.get_project_uid(proj_name=input_dict[uis.name], 
+                                        proj_type=input_dict[uis.ptype], 
+                                        proj_jur=input_dict[uis.jur], 
+                                        user_email=input_dict[uis.email])
+
+
+    # data_to_log = {
+    #     'project_uid': project_uid, 'base_trntrip': ,
+    #     'base_vehocc': , 'future_trntrip': ,
+    #     'future_vehocc': ,
+
+
+    # utils.log_row_to_table(data_row_dict=data_to_log, dest_table=os.path.join(params.log_fgdb, 'rp_fwy_vmt'))
 
     # write out to new JSON file
     output_sufx = str(dt.datetime.now().strftime('%Y%m%d_%H%M'))
@@ -77,24 +101,55 @@ if __name__ == '__main__':
 
     # ===========USER INPUTS THAT CHANGE WITH EACH PROJECT RUN============
 
-
-    # specify project line feature class and attributes
-    project_fc = arcpy.GetParameterAsText(0)  
-    project_name = arcpy.GetParameterAsText(1) # NOTE = IS HAVING A PROJECT NAME EVEN NEEDED FOR MOST OF THESE GP TASKS?
+    # inputs from tool interface
+    project_fc = arcpy.GetParameterAsText(0)
+    project_name = arcpy.GetParameterAsText(1)
+    jurisdiction = arcpy.GetParameterAsText(2)
+    project_type = arcpy.GetParameterAsText(3)
+    perf_outcomes = arcpy.GetParameterAsText(4)
+    aadt = arcpy.GetParameterAsText(5)
+    posted_spd = arcpy.GetParameterAsText(6)
+    pci = arcpy.GetParameterAsText(7)
+    email = arcpy.GetParameterAsText(8)
 
     # hard-coded vals for testing
-    # project_fc = r'I:\Projects\Darren\PPA3_GIS\PPA3Testing.gdb\TestBiz80curve' 
-    # project_name = 'Biz80Cuve'
+    project_fc = r'\\data-svr\GIS\Projects\Darren\PPA3_GIS\PPA3Testing.gdb\TestBroadway16th' # Broadway16th_2226
+    project_name = 'broadway'
+    jurisdiction = 'sac city'
+    project_type = params.ptype_arterial
+    perf_outcomes = 'TEST;Reduce Congestion;Reduce VMT'
+    aadt = 150000
+    posted_spd = 65
+    pci = 80
+    email = 'fake@test.com'
 
-    ptype = params.ptype_fwy
+    uis = params.user_inputs
+    input_parameter_dict = {
+        uis.geom: project_fc,
+        uis.name: project_name,
+        uis.jur: jurisdiction,
+        uis.ptype: project_type,
+        uis.perf_outcomes: perf_outcomes,
+        uis.aadt: aadt,
+        uis.posted_spd: posted_spd,
+        uis.pci: pci,
+        uis.email: email
+    }
     
 
     #=================BEGIN SCRIPT===========================
+    try:
+        arcpy.Delete_management(arcpy.env.scratchGDB) # ensures a new, fresh scratch GDB is created to avoid any weird file-not-found errors
+        print("Deleted arcpy scratch GDB to ensure reliability.")
+    except:
+        pass
+
+
     arcpy.env.workspace = params.fgdb
     output_dir = arcpy.env.scratchFolder
-    result_path = make_vmt_report_fwyexp(fc_project=project_fc, project_name=project_name, project_type=ptype)
+    result_path = make_vmt_report_fwyexp(input_dict=input_parameter_dict)
 
-    arcpy.SetParameterAsText(2, result_path) # clickable link to download file
+    arcpy.SetParameterAsText(9, result_path) # clickable link to download file
         
     arcpy.AddMessage(f"wrote JSON output to {result_path}")
 
