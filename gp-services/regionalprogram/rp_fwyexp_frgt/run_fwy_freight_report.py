89ba7624f3b7fa756bee254bdade6614ab7efe23
djconly85 <dconly@sacog.org>
2022-10-06 09:41:23 -0700

    publishing rp_fwyexp_freight gp task

--- gp-services/regionalprogram/rp_fwyexp_frgt/run_fwy_freight_report.py
+++ gp-services/regionalprogram/rp_fwyexp_frgt/run_fwy_freight_report.py
@@ -18,13 +18,18 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__))) # enable importing f
 import datetime as dt
 import json
 import arcpy
-
+arcpy.SetLogHistory(False) # prevents an XML log file from being created every time script is run; long terms saves hard drive space
 
 import parameters as params
 import get_truck_data_fwy as truckdata
+import utils.utils as utils
 
+def make_freight_rept_fwyexp(input_dict):
 
-def make_freight_rept_fwyexp(fc_project, project_name, project_type):
+    uis = params.user_inputs
+    fc_project=input_dict[uis.geom]
+    project_name=input_dict[uis.name]
+    project_type=input_dict[uis.ptype]
     
     in_json = os.path.join(params.json_templates_dir, "SACOG_{Regional Program}_{Freeway}_Freight_sample_dataSource.json")
 
@@ -39,6 +44,18 @@ def make_freight_rept_fwyexp(fc_project, project_name, project_type):
     output_data = truckdata.get_tmc_truck_data(fc_projline=fc_project, str_project_type=project_type)
     truck_pct = output_data[f"{params.col_truckpct}_proj"] / 100 
     loaded_json["Share of Traffic on Segment That is Trucks"] = truck_pct
+
+    # log to data table
+    project_uid = utils.get_project_uid(proj_name=input_dict[uis.name], 
+                                        proj_type=input_dict[uis.ptype], 
+                                        proj_jur=input_dict[uis.jur], 
+                                        user_email=input_dict[uis.email])
+
+    data_to_log = {
+        'project_uid': project_uid, 'pct_truckvol': truck_pct
+    }
+
+    utils.log_row_to_table(data_row_dict=data_to_log, dest_table=os.path.join(params.log_fgdb, 'rp_fwy_frgt'))
 
     # write out to new JSON file
     output_sufx = str(dt.datetime.now().strftime('%Y%m%d_%H%M'))
@@ -56,25 +73,55 @@ if __name__ == '__main__':
 
     # ===========USER INPUTS THAT CHANGE WITH EACH PROJECT RUN============
 
-
-    # specify project line feature class and attributes
+    # inputs from tool interface
     project_fc = arcpy.GetParameterAsText(0)
-    project_name = arcpy.GetParameterAsText(1) 
+    project_name = arcpy.GetParameterAsText(1)
+    jurisdiction = arcpy.GetParameterAsText(2)
+    project_type = arcpy.GetParameterAsText(3)
+    perf_outcomes = arcpy.GetParameterAsText(4)
+    aadt = arcpy.GetParameterAsText(5)
+    posted_spd = arcpy.GetParameterAsText(6)
+    pci = arcpy.GetParameterAsText(7)
+    email = arcpy.GetParameterAsText(8)
 
-    # hard values for testing
-    # project_fc = r'I:\Projects\Darren\PPA3_GIS\PPA3Testing.gdb\Test_I5SMF'
-    # project_name = 'test'
+    # hard-coded vals for testing
+    project_fc = r'\\data-svr\GIS\Projects\Darren\PPA3_GIS\PPA3Testing.gdb\Test_Causeway' # Broadway16th_2226
+    project_name = 'causeway'
+    jurisdiction = 'caltrans'
+    project_type = params.ptype_fwy
+    perf_outcomes = 'TEST;Reduce Congestion;Reduce VMT'
+    aadt = 150000
+    posted_spd = 65
+    pci = 80
+    email = 'fake@test.com'
 
-    ptype = params.ptype_fwy
+    uis = params.user_inputs
+    input_parameter_dict = {
+        uis.geom: project_fc,
+        uis.name: project_name,
+        uis.jur: jurisdiction,
+        uis.ptype: project_type,
+        uis.perf_outcomes: perf_outcomes,
+        uis.aadt: aadt,
+        uis.posted_spd: posted_spd,
+        uis.pci: pci,
+        uis.email: email
+    }
     
 
     #=================BEGIN SCRIPT===========================
+    try:
+        arcpy.Delete_management(arcpy.env.scratchGDB) # ensures a new, fresh scratch GDB is created to avoid any weird file-not-found errors
+        print("Deleted arcpy scratch GDB to ensure reliability.")
+    except:
+        pass
+
+
     arcpy.env.workspace = params.fgdb
     output_dir = arcpy.env.scratchFolder
-    result_path = make_freight_rept_fwyexp(fc_project=project_fc, project_name=project_name,
-                                        project_type=ptype)
+    result_path = make_freight_rept_fwyexp(input_dict=input_parameter_dict)
 
-    arcpy.SetParameterAsText(2, result_path) # clickable link to download file
+    arcpy.SetParameterAsText(9, result_path) # clickable link to download file
         
     arcpy.AddMessage(f"wrote JSON output to {result_path}")
 
