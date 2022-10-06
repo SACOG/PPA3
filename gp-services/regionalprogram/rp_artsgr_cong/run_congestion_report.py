a803e77e266f75e6831120b444b9a125fdba7f64
djconly85 <dconly@sacog.org>
2022-10-06 13:33:24 -0700

    published updated arterial sgr congestion gp task

--- gp-services/regionalprogram/rp_artsgr_cong/run_congestion_report.py
+++ gp-services/regionalprogram/rp_artsgr_cong/run_congestion_report.py
@@ -22,9 +22,52 @@ import arcpy
 import parameters as params
 import chart_congestion
 import npmrds_data_conflation as npmrds
+import utils.utils as utils
+
+def direction_field_translator(in_congdata_dict):
+
+    """Takes in dict with congestion data returned by npmrds_data_conflation module
+    and updates its key names so that they match the field names in the gdb table that the run's
+    congestion data will be logged to.
+    """
+    d_dirnames = {
+        'NORTHBOUND': 'nb',
+        'SOUTHBOUND': 'sb',
+        'EASTBOUND': 'eb',
+        'WESTBOUND': 'wb'
+    }
+
+    d_metrnames = {
+        'ff_speed': 'ffspd',
+        'havg_spd_worst4hrs': 'congspd',
+        'lottr_ampk': 'lottr_am',
+        'lottr_midday': 'lottr_md',
+        'lottr_pmpk': 'lottr_pm',
+        'lottr_wknd': 'lottr_wknd'
+    }
+
+    out_dict = {}
+    for dname_in, dname_out in d_dirnames.items():
+        for mname_in, mname_out in d_metrnames.items():
+            congdata_name = f"{dname_in}{mname_in}"
+            if congdata_name in in_congdata_dict.keys():
+                outval = in_congdata_dict[congdata_name]
+            else:
+                outval = None
+
+            outdata_name = f"{mname_out}_{dname_out}"
+
+            out_dict[outdata_name] = outval
+
+    return out_dict
 
 
-def make_congestion_rpt_artexp(fc_project, project_name, project_type, aadt):
+def make_congestion_rpt_artsgr(input_dict):
+
+    uis = params.user_inputs
+    fc_project = input_dict[uis.geom]
+    project_type = input_dict[uis.ptype]
+    aadt = input_dict[uis.aadt]
     
     in_json = os.path.join(params.json_templates_dir, "SACOG_{Regional Program}_{Arterial_SGR}_ReduceCongestion_sample_dataSource.json")
 
@@ -39,6 +82,23 @@ def make_congestion_rpt_artexp(fc_project, project_name, project_type, aadt):
 
     # update AADT
     loaded_json["projectAADT"] = aadt
+
+    # log data to run archive table
+    output_congn_data = direction_field_translator(in_congdata_dict=congn_data)
+
+    project_uid = utils.get_project_uid(proj_name=input_dict[uis.name], 
+                                        proj_type=input_dict[uis.ptype], 
+                                        proj_jur=input_dict[uis.jur], 
+                                        user_email=input_dict[uis.email])
+
+    data_to_log = {
+        'project_uid': project_uid, 'aadt': input_dict[uis.aadt]
+        }
+        
+    data_to_log.update(output_congn_data)
+
+    # NOTE that outputs for arterial sgr congestion report log to same table as those for arterial expansion congestion report.
+    utils.log_row_to_table(data_row_dict=data_to_log, dest_table=os.path.join(params.log_fgdb, 'rp_artexp_cong'))
 
     # write out to new JSON file
     output_sufx = str(dt.datetime.now().strftime('%Y%m%d_%H%M'))
@@ -56,26 +116,55 @@ if __name__ == '__main__':
 
     # ===========USER INPUTS THAT CHANGE WITH EACH PROJECT RUN============
 
+    # inputs from tool interface
+    project_fc = arcpy.GetParameterAsText(0)
+    project_name = arcpy.GetParameterAsText(1)
+    jurisdiction = arcpy.GetParameterAsText(2)
+    project_type = arcpy.GetParameterAsText(3)
+    perf_outcomes = arcpy.GetParameterAsText(4)
+    aadt = arcpy.GetParameterAsText(5)
+    posted_spd = arcpy.GetParameterAsText(6)
+    pci = arcpy.GetParameterAsText(7)
+    email = arcpy.GetParameterAsText(8)
 
-    # specify project line feature class and attributes
-    project_fc = arcpy.GetParameterAsText(0) # r'I:\Projects\Darren\PPA_V2_GIS\PPA_V2.gdb\TestTruxelBridge'  # 
-    project_name = arcpy.GetParameterAsText(1) # 'TestTruxelBridge' #  
-    proj_aadt = int(arcpy.GetParameterAsText(2)) # 32000 # 
+    # hard-coded vals for testing
+    # project_fc = r'\\data-svr\GIS\Projects\Darren\PPA3_GIS\PPA3Testing.gdb\TestBroadway16th'
+    # project_name = 'causeway'
+    # jurisdiction = 'Caltrans'
+    # project_type = params.ptype_sgr
+    # perf_outcomes = 'TEST;Reduce Congestion;Reduce VMT'
+    # aadt = 150000
+    # posted_spd = 65
+    # pci = 80
+    # email = 'fake@test.com'
 
-    # test values
-    # project_fc = r'I:\Projects\Darren\PPA3_GIS\PPA3Testing.gdb\TestJefferson'  # 
-    # project_name = 'Jefferson' #  
-    # proj_aadt = 32000 # 
-
-    ptype = params.ptype_arterial
+    uis = params.user_inputs
+    input_parameter_dict = {
+        uis.geom: project_fc,
+        uis.name: project_name,
+        uis.jur: jurisdiction,
+        uis.ptype: project_type,
+        uis.perf_outcomes: perf_outcomes,
+        uis.aadt: aadt,
+        uis.posted_spd: posted_spd,
+        uis.pci: pci,
+        uis.email: email
+    }
     
 
     #=================BEGIN SCRIPT===========================
+    try:
+        arcpy.Delete_management(arcpy.env.scratchGDB) # ensures a new, fresh scratch GDB is created to avoid any weird file-not-found errors
+        print("Deleted arcpy scratch GDB to ensure reliability.")
+    except:
+        pass
+
+
     arcpy.env.workspace = params.fgdb
     output_dir = arcpy.env.scratchFolder
-    result_path = make_congestion_rpt_artexp(fc_project=project_fc, project_name=project_name, project_type=ptype, aadt=proj_aadt)
+    result_path = make_congestion_rpt_artsgr(input_dict=input_parameter_dict)
 
-    arcpy.SetParameterAsText(3, result_path) # clickable link to download file
+    arcpy.SetParameterAsText(9, result_path) # clickable link to download file
         
     arcpy.AddMessage(f"wrote JSON output to {result_path}")
 
