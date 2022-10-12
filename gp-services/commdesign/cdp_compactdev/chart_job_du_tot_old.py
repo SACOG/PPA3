5b8e539a588f462c66831fbfcd8c0f26566a24c0
djconly85 <dconly@sacog.org>
2022-10-12 09:14:12 -0700

    published cd compact dev gp task

--- gp-services/commdesign/cdp_compactdev/chart_job_du_tot_old.py
+++ gp-services/commdesign/cdp_compactdev/chart_job_du_tot_old.py
@@ -13,15 +13,14 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__))) # enable importing from parent folder
 
-from landuse_buff_calcs import LandUseBuffCalcs
+import landuse_buff_calcs as lubuff
 import parameters as params
-
 
 def update_json(json_loaded, data_year, order_val, pcl_pt_fc, project_fc, project_type):
     value_fields = [params.col_emptot, params.col_du]
 
     out_data = {}
-    year_dict = LandUseBuffCalcs(pcl_pt_fc, project_fc, project_type, value_fields, 
+    year_dict = lubuff.LandUseBuffCalcs(pcl_pt_fc, project_fc, project_type, value_fields, 
                                 buffered_pcls=True).point_sum()
     out_data[data_year] = year_dict
 
@@ -32,8 +31,6 @@ def update_json(json_loaded, data_year, order_val, pcl_pt_fc, project_fc, projec
     json_loaded[params.k_charts][k_chart_name][params.k_features][order_val][params.k_attrs][params.k_year] = str(data_year) # need to convert to string for chart
     json_loaded[params.k_charts][k_chart_name][params.k_features][order_val][params.k_attrs]['jobs'] = jobs
     json_loaded[params.k_charts][k_chart_name][params.k_features][order_val][params.k_attrs]['dwellingUnits'] = du
-
-    return {f"jobs": jobs, f"dwellingUnits": du}
 
     print("calculated buffer values sucessfully")
 
