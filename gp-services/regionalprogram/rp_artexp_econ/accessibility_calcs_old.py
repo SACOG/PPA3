cb51a27041555992167982059088fa0f6ff23116
djconly85 <dconly@sacog.org>
2024-07-03 16:26:22 -0700

    progress on updating accessibility stuff (arterial econ GP task)

--- gp-services/regionalprogram/rp_artexp_econ/accessibility_calcs_old.py
+++ gp-services/regionalprogram/rp_artexp_econ/accessibility_calcs_old.py
@@ -12,73 +12,41 @@
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from time import perf_counter as perf
 
-import pandas as pd
+
+from time import perf_counter as perf
 import arcpy
-import numpy as np
-import geopandas as gpd
-import rasterio
-from rasterio.mask import mask
 
 import parameters as params
 from utils import utils as ut
 
 
-def get_raster_pts_near_line(tif, line_fc, mode, dest_type):
-
-    # load tif data
-    with rasterio.open(tif) as tifdata:
-        epsg_id = tifdata.crs.to_epsg()
-        crs_linunits = tifdata.crs.linear_units # placeholder in case you ever want to know what units the CRS uses.
-        crs_to_use = f"EPSG:{epsg_id}"
-        vals_masked, valmasked_transform = mask(tifdata, shapes=[mask_geom], all_touched=True, 
-                                                crop=True, pad=True, pad_width=0.5)
-        vals_array = vals_masked[0]
-
-    # create buffered line object around project line
-    line_gdf = ut.esri_to_df(esri_obj_path=line_fc, include_geom=True, field_list=None, index_field=None, 
-                   crs_val=crs_to_use, dissolve=False)
-    line_gdf['geometry'] = line_gdf['geometry'].buffer(100) # distance in meters if EPSG 3857 (web mercator)
-
-    mask_geom = line_gdf.geometry[0]
-
-    # make vector point gdf from the pixels that are near the line. Will need to further trim to go from
-    # rectangle of points to just points that are right along the line.
-    # helpful site = https://gis.stackexchange.com/questions/388047/get-coordinates-of-all-pixels-in-a-raster-with-rasterio
-    row_range = np.arange(vals_array.shape[0])
-    col_range = np.arange(vals_array.shape[1])
-
-    coord_arr = []
-    valname = f"{mode}_{dest_type}"
-    for r in row_range:
-        for c in col_range:
-            x, y = rasterio.transform.xy(valmasked_transform, r, c)
-            val = vals_array[r][c]
-            coord_arr.append({'cellid': f"{r}_{c}", 'x': x, 'y': y, valname: val})
-
-    df = pd.DataFrame(coord_arr)
-    gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.x, df.y), crs=crs_to_use)
-    
-    return gdf
-
-
-def get_acc_data(fc_project, tif_accdata, tif_weights, project_type, get_ej=False):
+def get_acc_data(fc_project, fc_accdata, project_type, get_ej=False):
     '''Calculate average accessibility to selected destination types for all
     polygons that either intersect the project line or are within a community type polygon.
     Average accessibility is weighted by each polygon's population.'''
     
     arcpy.AddMessage("Calculating accessibility metrics...")
+    
+    sufx = int(perf()) + 1
+    fl_accdata = os.path.join('memory','fl_accdata{}'.format(sufx))
+    fl_project = 'fl_project'
 
-    # load tifs and trim to those near project line
-    gdf_acc = get_raster_pts_near_line(tif_accdata, fc_project, mode=, dest_type=)
-    gdf_wt = get_raster_pts_near_line(tif_weights, fc_project, mode=, dest_type=)
+    if arcpy.Exists(fl_project): arcpy.Delete_management(fl_project)
+    arcpy.MakeFeatureLayer_management(fc_project, fl_project)
+
+    if arcpy.Exists(fl_accdata): arcpy.Delete_management(fl_accdata)
+    arcpy.MakeFeatureLayer_management(fc_accdata, fl_accdata)
+
+    # select polygons that intersect with the project line
+    searchdist = 0 if project_type == params.ptype_area_agg else params.bg_search_dist
+    arcpy.SelectLayerByLocation_management(fl_accdata, "INTERSECT", fl_project, searchdist, "NEW_SELECTION")
+
+    # read accessibility data from selected polygons into a dataframe
+    accdata_fields = [params.col_geoid, params.col_acc_ej_ind, params.col_pop] + params.acc_cols_ej
+    accdata_df = ut.esri_object_to_df(fl_accdata, accdata_fields)
 
     # get pop-weighted accessibility values for all accessibility columns
-    gdf_jn = 
-
-
-    # 7/3/2024 - WILL YOU WANT TO GET "ACCESS FOR EJ POPULATIONS" METRIC? IDEALLY COULD REMOVE IT; DOES NOT REALLY TELL US ANYTHING USEFUL.
 
     out_dict = {}
     if get_ej: # if for enviro justice population, weight by population for EJ polygons only.
