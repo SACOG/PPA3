9193f9ed53f25f262c956528e346dd549ab8516c
djconly85 <dconly@sacog.org>
2024-07-03 17:00:55 -0700

    more work on accessibility calcs

--- gp-services/regionalprogram/rp_artexp_econ/accessibility_calcs.py
+++ gp-services/regionalprogram/rp_artexp_econ/accessibility_calcs.py
@@ -10,6 +10,7 @@
 # Python Version: 3.x
 # --------------------------------
 import os
+from pathlib import Path
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from time import perf_counter as perf
@@ -24,8 +25,13 @@ from rasterio.mask import mask
 import parameters as params
 from utils import utils as ut
 
+import yaml
+yaml_file = os.path.join(os.path.dirname(__file__), 'data_paths.yaml')
+with open(yaml_file, 'r') as y:
+    pathconfigs = yaml.load(y, Loader=yaml.FullLoader)
 
-def get_raster_pts_near_line(tif, line_fc, mode, dest_type):
+
+def get_raster_pts_near_line(tif, line_fc, valname, search_dist_m=100):
 
     # load tif data
     with rasterio.open(tif) as tifdata:
@@ -50,7 +56,6 @@ def get_raster_pts_near_line(tif, line_fc, mode, dest_type):
     col_range = np.arange(vals_array.shape[1])
 
     coord_arr = []
-    valname = f"{mode}_{dest_type}"
     for r in row_range:
         for c in col_range:
             x, y = rasterio.transform.xy(valmasked_transform, r, c)
@@ -63,47 +68,60 @@ def get_raster_pts_near_line(tif, line_fc, mode, dest_type):
     return gdf
 
 
-def get_acc_data(fc_project, tif_accdata, tif_weights, project_type, get_ej=False):
+def get_acc_data(fc_project, tif_weights, project_type, get_ej=False):
     '''Calculate average accessibility to selected destination types for all
     polygons that either intersect the project line or are within a community type polygon.
     Average accessibility is weighted by each polygon's population.'''
     
     arcpy.AddMessage("Calculating accessibility metrics...")
 
-    # load tifs and trim to those near project line
-    gdf_acc = get_raster_pts_near_line(tif_accdata, fc_project, mode=, dest_type=)
-    gdf_wt = get_raster_pts_near_line(tif_weights, fc_project, mode=, dest_type=)
-
-    # get pop-weighted accessibility values for all accessibility columns
-    gdf_jn = 
-
+    # load tif of population used for weighting
+    searchdist = 0 if project_type == params.ptype_area_agg else params.acc_search_dist
+    wt = 'pop'
+    gdf_wt = get_raster_pts_near_line(tif_weights, fc_project, valname=wt, search_dist_m=searchdist)
 
     # 7/3/2024 - WILL YOU WANT TO GET "ACCESS FOR EJ POPULATIONS" METRIC? IDEALLY COULD REMOVE IT; DOES NOT REALLY TELL US ANYTHING USEFUL.
+    
 
     out_dict = {}
-    if get_ej: # if for enviro justice population, weight by population for EJ polygons only.
-        for col in params.acc_cols_ej:
-            col_wtd = "{}_wtd".format(col)
-            col_ej_pop = "{}_EJ".format(params.col_pop)
-            accdata_df[col_wtd] = accdata_df[col] * accdata_df[params.col_pop] * accdata_df[params.col_acc_ej_ind]
-            accdata_df[col_ej_pop] = accdata_df[params.col_pop] * accdata_df[params.col_acc_ej_ind]
+    acclayer_dict = pathconfigs['server_data']['access_layers']
+    acclayers_dir = Path(pathconfigs['server_data']['acc_dir'])
+    for dest in acclayer_dict.keys():
+        for mode in acclayer_dict[dest]:
+            i_dict_key = f"{mode}_{dest}"
+            if gdfjn[wt].sum() == 0: # if no people, get unweighted avg access
+                wtd_avg = gdfjn[dest].mean() 
+            else: # otherwise get pop-weighted average -- NOTE may want to improve this so you can specify different weight for each destination type
+                acc_tif = str(acclayers_dir.joinpath(acclayer_dict[dest][mode]))
+                gdf_acc = get_raster_pts_near_line(acc_tif, fc_project, val_name=i_dict_key, search_dist_m=searchdist)
+                gdfjn = gdf_acc.merge(gdf_wt, on='cellid')
+                wtd_avg = (gdfjn[dest]*gdfjn[wt]).sum() / gdfjn[wt].sum()
+
+            out_dict[i_dict_key] = wtd_avg
+
+    # if get_ej: # if for enviro justice population, weight by population for EJ polygons only.
+    #     for col in params.acc_cols_ej:
+    #         col_wtd = "{}_wtd".format(col)
+    #         col_ej_pop = "{}_EJ".format(params.col_pop)
+    #         accdata_df[col_wtd] = accdata_df[col] * accdata_df[params.col_pop] * accdata_df[params.col_acc_ej_ind]
+    #         accdata_df[col_ej_pop] = accdata_df[params.col_pop] * accdata_df[params.col_acc_ej_ind]
             
-            tot_ej_pop = accdata_df[col_ej_pop].sum()
+    #         tot_ej_pop = accdata_df[col_ej_pop].sum()
             
-            out_wtd_acc = accdata_df[col_wtd].sum() / tot_ej_pop if tot_ej_pop > 0 else 0
-            col_out_ej = "{}_EJ".format(col)
-            out_dict[col_out_ej] = out_wtd_acc
-    else:
-        total_pop = accdata_df[params.col_pop].sum()
-        for col in params.acc_cols:
-            if total_pop <= 0: # if no one lives near project, get unweighted avg accessibility of block groups near project
-                out_wtd_acc = accdata_df[col].mean()
-            else:
-                col_wtd = "{}_wtd".format(col)
-                accdata_df[col_wtd] = accdata_df[col] * accdata_df[params.col_pop]
-                out_wtd_acc = accdata_df[col_wtd].sum() / total_pop
+    #         out_wtd_acc = accdata_df[col_wtd].sum() / tot_ej_pop if tot_ej_pop > 0 else 0
+    #         col_out_ej = "{}_EJ".format(col)
+    #         out_dict[col_out_ej] = out_wtd_acc
+    # else:
+    #     total_pop = accdata_df[params.col_pop].sum()
+    #     for col in params.acc_cols:
+    #         if total_pop <= 0: # if no one lives near project, get unweighted avg accessibility of block groups near project
+    #             out_wtd_acc = accdata_df[col].mean()
+    #         else:
+    #             col_wtd = "{}_wtd".format(col)
+    #             accdata_df[col_wtd] = accdata_df[col] * accdata_df[params.col_pop]
+    #             out_wtd_acc = accdata_df[col_wtd].sum() / total_pop
                 
-            out_dict[col] = out_wtd_acc
+    #         out_dict[col] = out_wtd_acc
 
     return out_dict
 
@@ -111,8 +129,7 @@ def get_acc_data(fc_project, tif_accdata, tif_weights, project_type, get_ej=Fals
 if __name__ == '__main__':
     arcpy.env.workspace = r'I:\Projects\Darren\PPA_V2_GIS\PPA_V2.gdb'
     
-    fc_project_line = r'I:\Projects\Darren\PPA_V2_GIS\PPA_V2.gdb\Polylines'
-    fc_accessibility_data = params.accdata_fc
+    fc_project_line = r"\\data-svr\GIS\Projects\Darren\PPA3_GIS\PPA3Testing.gdb\Test_Causeway"
     str_project_type = params.ptype_arterial
     
     dict_data = get_acc_data(fc_project_line, fc_accessibility_data, str_project_type)
