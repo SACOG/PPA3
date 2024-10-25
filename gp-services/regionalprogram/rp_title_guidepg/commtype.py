f4a1ecba95493f29b26cfcd9f7defd7fee4a8486
djconly85 <dconly@sacog.org>
2024-10-25 16:24:20 -0700

    working commtype script for rpt title page task

--- gp-services/regionalprogram/rp_title_guidepg/commtype.py
+++ gp-services/regionalprogram/rp_title_guidepg/commtype.py
@@ -35,7 +35,7 @@ def get_ctype_from_ix(ix_fc, quant_field):
                 ctype_size_dict[ctype] = piece_size
             else:
                 ctype_size_dict[ctype] += piece_size
-                
+
     maxval = max([v for k, v in ctype_size_dict.items()])
     proj_ctype = [k for k, v in ctype_size_dict.items() if v == maxval][0]
 
@@ -71,41 +71,9 @@ def get_proj_ctype(in_project_fc, commtypes_fc):
             raise ValueError("ERROR: No Community Type identified for project. \n{} project line features." \
                              " {} features in intersect layer.".format(in_project_cnt, intersect_cnt))
         else:
-            proj_ctype = get_ctype_from_ix(temp_intersect_fc, quant_field='SHAPE@AREA')
+            proj_ctype = get_ctype_from_ix(temp_pbuff_ix_fc, quant_field='SHAPE@AREA')
         
     return proj_ctype
-
-
-# def get_proj_ctype(in_project_fc, commtypes_fc):
-#     '''Get project community type, based on which community type has most spatial overlap with project'''
-#     ts = int(perf())
-#     temp_intersect_fc = os.path.join('memory', f'temp_intersect_fc{ts}')
-#     if arcpy.Exists(temp_intersect_fc): arcpy.Delete_management(temp_intersect_fc)
-
-#     arcpy.Intersect_analysis([in_project_fc, commtypes_fc], temp_intersect_fc, "ALL", 0, "LINE")
-    
-#     # debugging messages to find out why ctype tagging intermittently fails
-#     intersect_cnt = int(arcpy.GetCount_management(temp_intersect_fc)[0])
-#     in_project_cnt = int(arcpy.GetCount_management(in_project_fc)[0])
-    
-
-#     with arcpy.da.SearchCursor(temp_intersect_fc, fields) as cur:
-#         for row in cur:
-#             ctype = row[fields.index(params.col_ctype)]
-#             seg_len = row[fields.index(len_field)]
-        
-#             if ctype_dist_dict.get(ctype) is None:
-#                 ctype_dist_dict[ctype] = seg_len
-#             else:
-#                 ctype_dist_dict[ctype] += seg_len
-#     try:
-#         maxval = max([v for k, v in ctype_dist_dict.items()])
-#         proj_ctype = [k for k, v in ctype_dist_dict.items() if v == maxval][0]
-
-#         return proj_ctype
-#     except:
-#         raise ValueError("ERROR: No Community Type identified for project. \n{} project line features." \
-#         " {} features in intersect layer.".format(in_project_cnt, intersect_cnt))
 
 
 
