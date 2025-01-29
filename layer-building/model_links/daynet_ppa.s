5195fd634ad0c63b3796a91c2aa97bed6a7d783e
djconly85 <dconly@sacog.org>
2025-01-29 13:10:38 -0800

    working model link layer builder

--- layer-building/model_links/daynet_ppa.s
+++ layer-building/model_links/daynet_ppa.s
@@ -9,10 +9,7 @@ filei linki[1]=?_base.net
       linki[8]=vo.h17.net
       linki[9]=vo.ev2.net
       linki[10]=vo.n11.net
-fileo neto=?day_ghg.net include=bike,tollda,tollse,tolls3,prevvol,prevvol,vc_1,cspd_1,
-             vdt_1,vht_1,v1_1,v2_1,v3_1,vt_1,v1t_1,v2t_1,v3t_1
-      linko=?daynet_vmt.dbf format=dbf include = A B NAME DISTANCE CAPCLASS LANES SPEED LANEMI,
-											DYV DAYVMT DAYCVMT DAYCV DYV_DA DYV_SD2 DYV_SD3 DYV_CV
+
 
 ;if (capclass=7,99) delete
 county='NA'
@@ -26,33 +23,69 @@ if (rad=97) county='External'
 
 lanemi=lanes*distance
 
-; link volumes
 
-; link volumes
-h07v = li.2.V1_1	+	li.2.V2_1	+	li.2.V3_1	+	li.2.V4_1	+	li.2.V5_1	+	li.2.V6_1	+	li.2.V7_1	+	li.2.V8_1	+	li.2.V9_1	+	li.2.V10_1	+	li.2.V11_1	+	
-       li.2.V12_1	+	li.2.V13_1	+	li.2.V14_1	+	li.2.V15_1
-h08v = li.3.V1_1	+	li.3.V2_1	+	li.3.V3_1	+	li.3.V4_1	+	li.3.V5_1	+	li.3.V6_1	+	li.3.V7_1	+	li.3.V8_1	+	li.3.V9_1	+	li.3.V10_1	+	li.3.V11_1	+	
-       li.3.V12_1	+	li.3.V13_1	+	li.3.V14_1	+	li.3.V15_1
-h09v = li.4.V1_1	+	li.4.V2_1	+	li.4.V3_1	+	li.4.V4_1	+	li.4.V5_1	+	li.4.V6_1	+	li.4.V7_1	+	li.4.V8_1	+	li.4.V9_1	+	li.4.V10_1	+	li.4.V11_1	+	
-       li.4.V12_1	+	li.4.V13_1	+	li.4.V14_1	+	li.4.V15_1
-md5v = li.5.V1_1	+	li.5.V2_1	+	li.5.V3_1	+	li.5.V4_1	+	li.5.V5_1	+	li.5.V6_1	+	li.5.V7_1	+	li.5.V8_1	+	li.5.V9_1	+	li.5.V10_1	+	li.5.V11_1	+	
-       li.5.V12_1	+	li.5.V13_1	+	li.5.V14_1	+	li.5.V15_1
-h15v = li.6.V1_1	+	li.6.V2_1	+	li.6.V3_1	+	li.6.V4_1	+	li.6.V5_1	+	li.6.V6_1	+	li.6.V7_1	+	li.6.V8_1	+	li.6.V9_1	+	li.6.V10_1	+	li.6.V11_1	+	
-       li.6.V12_1	+	li.6.V13_1	+	li.6.V14_1	+	li.6.V15_1
-h16v = li.7.V1_1	+	li.7.V2_1	+	li.7.V3_1	+	li.7.V4_1	+	li.7.V5_1	+	li.7.V6_1	+	li.7.V7_1	+	li.7.V8_1	+	li.7.V9_1	+	li.7.V10_1	+	li.7.V11_1	+	
-       li.7.V12_1	+	li.7.V13_1	+	li.7.V14_1	+	li.7.V15_1
-h17v = li.8.V1_1	+	li.8.V2_1	+	li.8.V3_1	+	li.8.V4_1	+	li.8.V5_1	+	li.8.V6_1	+	li.8.V7_1	+	li.8.V8_1	+	li.8.V9_1	+	li.8.V10_1	+	li.8.V11_1	+	
-       li.8.V12_1	+	li.8.V13_1	+	li.8.V14_1	+	li.8.V15_1
-ev2v = li.9.V1_1	+	li.9.V2_1	+	li.9.V3_1	+	li.9.V4_1	+	li.9.V5_1	+	li.9.V6_1	+	li.9.V7_1	+	li.9.V8_1	+	li.9.V9_1	+	li.9.V10_1	+	li.9.V11_1	+	
-       li.9.V12_1	+	li.9.V13_1	+	li.9.V14_1	+	li.9.V15_1
-n11v = li.10.V1_1	+	li.10.V2_1	+	li.10.V3_1	+	li.10.V4_1	+	li.10.V5_1	+	li.10.V6_1	+	li.10.V7_1	+	li.10.V8_1	+	li.10.V9_1	+	li.10.V10_1	+
-       li.10.V11_1	+	li.10.V12_1	+	li.10.V13_1	+	li.10.V14_1	+	li.10.V15_1
+; link volume by user type
+h07v_da = li.2.V1_1 + li.2.V6_1 + li.2.V11_1
+h07v_sd2 = li.2.V4_1 + li.2.V9_1 + li.2.V14_1
+h07v_sd3 = li.2.V5_1 + li.2.V10_1 + li.2.V15_1
+h07v_cv = li.2.V2_1 + li.2.V3_1 + li.2.V7_1 + li.2.V8_1 + li.2.V12_1 + li.2.V13_1
+h07v = h07v_da + h07v_sd2 + h07v_sd3 + h07v_cv
 
-a3v=int(h07v+h08v+h09v)
-mdv=int(md5v)
-p3v=int(h15v+h16v+h17v)
-evv=int(ev2v+n11v)
-dyv=int(a3v+mdv+p3v+evv)
+h08v_da = li.3.V1_1 + li.3.V6_1 + li.3.V11_1
+h08v_sd2 = li.3.V4_1 + li.3.V9_1 + li.3.V14_1
+h08v_sd3 = li.3.V5_1 + li.3.V10_1 + li.3.V15_1
+h08v_cv = li.3.V2_1 + li.3.V3_1 + li.3.V7_1 + li.3.V8_1 + li.3.V12_1 + li.3.V13_1
+h08v = h08v_da + h08v_sd2 + h08v_sd3 + h08v_cv
+
+h09v_da = li.4.V1_1 + li.4.V6_1 + li.4.V11_1
+h09v_sd2 = li.4.V4_1 + li.4.V9_1 + li.4.V14_1
+h09v_sd3 = li.4.V5_1 + li.4.V10_1 + li.4.V15_1
+h09v_cv = li.4.V2_1 + li.4.V3_1 + li.4.V7_1 + li.4.V8_1 + li.4.V12_1 + li.4.V13_1
+h09v = h09v_da + h09v_sd2 + h09v_sd3 + h09v_cv
+
+md5v_da = li.5.V1_1 + li.5.V6_1 + li.5.V11_1
+md5v_sd2 = li.5.V4_1 + li.5.V9_1 + li.5.V14_1
+md5v_sd3 = li.5.V5_1 + li.5.V10_1 + li.5.V15_1
+md5v_cv = li.5.V2_1 + li.5.V3_1 + li.5.V7_1 + li.5.V8_1 + li.5.V12_1 + li.5.V13_1
+md5v = md5v_da + md5v_sd2 + md5v_sd3 + md5v_cv
+
+h15v_da = li.6.V1_1 + li.6.V6_1 + li.6.V11_1
+h15v_sd2 = li.6.V4_1 + li.6.V9_1 + li.6.V14_1
+h15v_sd3 = li.6.V5_1 + li.6.V10_1 + li.6.V15_1
+h15v_cv = li.6.V2_1 + li.6.V3_1 + li.6.V7_1 + li.6.V8_1 + li.6.V12_1 + li.6.V13_1
+h15v = h15v_da + h15v_sd2 + h15v_sd3 + h15v_cv
+
+h16v_da = li.7.V1_1 + li.7.V6_1 + li.7.V11_1
+h16v_sd2 = li.7.V4_1 + li.7.V9_1 + li.7.V14_1
+h16v_sd3 = li.7.V5_1 + li.7.V10_1 + li.7.V15_1
+h16v_cv = li.7.V2_1 + li.7.V3_1 + li.7.V7_1 + li.7.V8_1 + li.7.V12_1 + li.7.V13_1
+h16v = h16v_da + h16v_sd2 + h16v_sd3 + h16v_cv
+
+h17v_da = li.8.V1_1 + li.8.V6_1 + li.8.V11_1
+h17v_sd2 = li.8.V4_1 + li.8.V9_1 + li.8.V14_1
+h17v_sd3 = li.8.V5_1 + li.8.V10_1 + li.8.V15_1
+h17v_cv = li.8.V2_1 + li.8.V3_1 + li.8.V7_1 + li.8.V8_1 + li.8.V12_1 + li.8.V13_1
+h17v = h17v_da + h17v_sd2 + h17v_sd3 + h17v_cv
+
+ev2v_da = li.9.V1_1 + li.9.V6_1 + li.9.V11_1
+ev2v_sd2 = li.9.V4_1 + li.9.V9_1 + li.9.V14_1
+ev2v_sd3 = li.9.V5_1 + li.9.V10_1 + li.9.V15_1
+ev2v_cv = li.9.V2_1 + li.9.V3_1 + li.9.V7_1 + li.9.V8_1 + li.9.V12_1 + li.9.V13_1
+ev2v = ev2v_da + ev2v_sd2 + ev2v_sd3 + ev2v_cv
+
+n11v_da = li.10.V1_1 + li.10.V6_1 + li.10.V11_1
+n11v_sd2 = li.10.V4_1 + li.10.V9_1 + li.10.V14_1
+n11v_sd3 = li.10.V5_1 + li.10.V10_1 + li.10.V15_1
+n11v_cv = li.10.V2_1 + li.10.V3_1 + li.10.V7_1 + li.10.V8_1 + li.10.V12_1 + li.10.V13_1
+n11v = n11v_da + n11v_sd2 + n11v_sd3 + n11v_cv
+
+
+dyv_da = h07v_da + h08v_da + h09v_da + md5v_da + h15v_da + h16v_da + h17v_da + ev2v_da + n11v_da
+dyv_sd2 = h07v_sd2 + h08v_sd2 + h09v_sd2 + md5v_sd2 + h15v_sd2 + h16v_sd2 + h17v_sd2 + ev2v_sd2 + n11v_sd2
+dyv_sd3 = h07v_sd3 + h08v_sd3 + h09v_sd3 + md5v_sd3 + h15v_sd3 + h16v_sd3 + h17v_sd3 + ev2v_sd3 + n11v_sd3
+dyv_cv = h07v_cv + h08v_cv + h09v_cv + md5v_cv + h15v_cv + h16v_cv + h17v_cv + ev2v_cv + n11v_cv
+
+dyv = dyv_da + dyv_sd2 + dyv_sd3 + dyv_cv
 
 ; link v/c ratios
 h07vc = li.2.vc_1
@@ -64,6 +97,7 @@ h16vc = li.7.vc_1
 h17vc = li.8.vc_1
 ev2vc = li.9.vc_1
 n11vc = li.10.vc_1
+
 ; link vmt
 h07vmt = h07v*distance
 h08vmt = h08v*distance
@@ -114,39 +148,30 @@ evcv=0
 daycv=0
 if(h07vc>1.0)
    h07cvmt=h07vmt
-   h07cv=h07v
 endif
 if(h08vc>1.0)
    h08cvmt=h08vmt
-   h08cv=h08v
 endif
 if(h09vc>1.0)
    h09cvmt=h09vmt
-   h09cv=h09v
 endif
 if(md5vc>1.0)
    md5cvmt=md5vmt
-   md5cv=md5v
 endif
 if(h15vc>1.0)
    h15cvmt=h15vmt
-   h15cv=h15v
 endif
 if(h16vc>1.0)
    h16cvmt=h16vmt
-   h16cv=h16v
 endif
 if(h17vc>1.0)
    h17cvmt=h17vmt
-   h17cv=h17v
 endif
 if(ev2vc>1.0)
    ev2cvmt=ev2vmt
-   ev2cv=ev2v
 endif
 if(n11vc>1.0)
    n11cvmt=n11vmt
-   n11cv=n11v
 endif
 
 a3cvmt=h07cvmt+h08cvmt+h09cvmt
@@ -154,13 +179,11 @@ mdcvmt=md5cvmt
 p3cvmt=h15cvmt+h16cvmt+h17cvmt
 evcvmt=ev2cvmt+n11cvmt
 
-a3cv=h07cv+h08cv+h09cv
-p3cv=h15cv+h16cv+h17cv
-mdcv=md5cv
-evcv=ev2cv+n11cv
-
 daycvmt=a3cvmt+p3cvmt+mdcvmt+evcvmt
-daycv=a3cv+p3cv+mdcv+evcv
+
+fileo neto=?daynet_ppa.net,
+			INCLUDE=A B NAME DISTANCE CAPCLASS LANES SPEED LANEMI,
+					DAYVMT DAYCVMT DYV_DA DYV_SD2 DYV_SD3 DYV_CV DYV
 
 merge record=false
 endrun
--- layer-building/model_links/ppa_model_links.py
+++ layer-building/model_links/ppa_model_links.py
@@ -9,11 +9,89 @@ Updated by:
 Copyright:   (c) SACOG
 Python Version: 3.x
 """
+from pathlib import Path
 
+import arcpy
+import pandas as pd
+from dbfread import DBF
 
-import netpyconvert as npc
+from netpyconvert import netpyconvert as npc
 
+def dbf2df(dbf_path, fields_to_load):
+	# maybe faster, and more memory-efficient, way to load big DBF into pandas dataframe
+	dbf_obj = DBF(dbf_path)
+
+	data_rows = []
+	for row in dbf_obj:
+		row2append = {}
+		for fname, fval in row.items():
+			if fname in fields_to_load: row2append[fname] = fval
+		data_rows.append(row2append)
+	
+	df_out = pd.DataFrame(data_rows)
+
+	return df_out
+
+def get_model_link_data(run_folder, scen_year, daynet_net, result_gdb):
+
+    arcpy.env.outputCoordinateSystem = arcpy.SpatialReference(2226)
+    arcpy.env.overwriteOutput = True
+    if arcpy.Exists(arcpy.env.scratchFolder):
+        arcpy.Delete_management(arcpy.env.scratchFolder)
+
+    f_nodea = 'A'
+    f_nodeb = 'B'
+
+    # convert daynet to SHP then export to GIS database as feature class
+    temp_shp = str(Path(arcpy.env.scratchFolder).joinpath("TEMP_model_links.shp"))
+    temp_shp = npc.net2linkshp(in_net_path=daynet_net, scenario_prefix=scen_year, out_link_path=temp_shp)
+    
+    out_fc = str(Path(result_gdb).joinpath(f"model_links_{scen_year}"))
+    sql_real_roads = "CAPCLASS < 62" # exclude non-real links like centroid connectors, disabled links, etc.
+    arcpy.ExportFeatures_conversion(temp_shp, out_fc, where_clause=sql_real_roads)
+
+    # get transit vol data on each link
+    f_trnvol = 'TOT_TRNVOL'
+    print("adding transit volume data to model links...")
+    trndata_dbf = Path(run_folder).joinpath(f"trans.link.all.dbf")
+    trn_cols = [f_nodea, f_nodeb, 'VOL', 'REV_VOL']
+    df_trndata = dbf2df(dbf_path=trndata_dbf, fields_to_load=trn_cols)
+    df_summed = df_trndata.groupby([f_nodea, f_nodeb]).sum().reset_index()
+    df_summed[f_trnvol] = df_summed['VOL'] + df_summed['REV_VOL']
+    df_summed['LINKID'] = df_summed[f_nodea].astype('str') + '_' + df_summed[f_nodeb].astype('str')
+    del df_summed['VOL'], df_summed['REV_VOL'], df_summed[f_nodea], df_summed[f_nodeb]
+
+    trn_data = {i['LINKID']: i['TOT_TRNVOL'] for i in df_summed.to_dict('records')}
+
+    # add field for trn vol to model links
+    print("adding transit data field...")
+    if f_trnvol in [f.name for f in arcpy.ListFields(out_fc)]:
+        arcpy.DeleteField_management(out_fc, f_trnvol)
+
+    arcpy.AddField_management(out_fc, f_trnvol, 'DOUBLE')
+
+    # join transit vol data to link data
+    print("adding transit data...")
+    try:
+        with arcpy.da.UpdateCursor(out_fc, field_names=[f_nodea, f_nodeb, f_trnvol]) as ucur:
+            for row in ucur:
+                linkid = f"{row[0]}_{row[1]}"
+                tvol = trn_data.get(linkid)
+                if not tvol: tvol = 0 # if link not found in transit file, assume it has no transit service on it.
+                row[2] = tvol
+                ucur.updateRow(row)
+    except:
+        import pdb; pdb.set_trace()
+
+    return out_fc
+                
 
 
 if __name__ == '__main__':
-    daynet = 
+    daynet = r"\\win11-model-2\D\SACSIM23\2020\2020_60_SOV69.5_Tele20\run_folder\2020daynet_ppa.net"
+    sc_year = 2020
+    result_fgdb = r'I:\Projects\Darren\PPA3_GIS\PPA3_GIS.gdb'
+    run_dir = str(Path(daynet).parent)
+
+    result = get_model_link_data(run_dir, sc_year, daynet, result_fgdb)
+    print(result)
