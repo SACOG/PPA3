394be6aa64fcc689fb70c344a1944fb5092c5c00
djconly85 <dconly@sacog.org>
2025-01-29 17:07:29 -0800

    additional yaml updates

--- layer-building/centerline_intersection/FindIntersections.py
+++ layer-building/centerline_intersection/FindIntersections.py
@@ -11,27 +11,35 @@
 import arcpy, os
 
 # Inputs
-allstreetlyr = r'I:\Projects\Darren\PPA3_GIS\PPA3_GIS.gdb\RegionalCenterline_Oct2021'
-output = "I:\Projects\Darren\PPA3_GIS\PPA3_GIS.gdb\intersections_2022"
+fc_allstreet = r'I:\SDE_Connections\SDE-PPA\owner@PPA.sde\OWNER.RegionalCenterline_2024'
+output = "I:\Projects\Darren\PPA3_GIS\PPA3_GIS.gdb\intersections_2024"
 # Enter as True or False
 RemoveTwoWayInts = True
+RemoveRampLinks = True 
 KeepCulDeSac = False
 
 #####################################################
 
-arcpy.env.workspace = os.path.dirname(allstreetlyr)
+arcpy.env.workspace = os.path.dirname(fc_allstreet)
 arcpy.env.overwriteOutput = True
 
 print("converting features to intersection points...")
+fl_allstreet = 'fl_allstreet'
+arcpy.MakeFeatureLayer_management(fc_allstreet, fl_allstreet)
+
+if RemoveRampLinks:
+    qry_noramps = "CLASS != 'RAMP'"
+    arcpy.SelectLayerByAttribute_management(fl_allstreet, selection_type="NEW_SELECTION", where_clause=qry_noramps)
+
 if KeepCulDeSac:
-    Int1_lyr = arcpy.FeatureVerticesToPoints_management(allstreetlyr, os.path.join(arcpy.env.scratchGDB, "Int1_lyr"), "BOTH_ENDS")
+    Int1_lyr = arcpy.FeatureVerticesToPoints_management(fl_allstreet, os.path.join(arcpy.env.scratchGDB, "Int1_lyr"), "BOTH_ENDS")
     arcpy.AddMessage("Keeping Cul De Sacs")
 else:
-    Int1_lyr = arcpy.Intersect_analysis(allstreetlyr, os.path.join(arcpy.env.scratchGDB, "Int1_lyr"), "ALL", "", "POINT")
+    Int1_lyr = arcpy.Intersect_analysis(fl_allstreet, os.path.join(arcpy.env.scratchGDB, "Int1_lyr"), "ALL", "", "POINT")
 arcpy.DeleteIdentical_management(Int1_lyr, "Shape")
 
 print("spatial-joining street network layer onto intersections layer...")
-Int2_lyr = arcpy.SpatialJoin_analysis(Int1_lyr, allstreetlyr, os.path.join(arcpy.env.scratchGDB, "Int2_lyr"),"JOIN_ONE_TO_ONE")
+Int2_lyr = arcpy.SpatialJoin_analysis(Int1_lyr, fl_allstreet, os.path.join(arcpy.env.scratchGDB, "Int2_lyr"),"JOIN_ONE_TO_ONE")
 whereclause3 = ""
 if RemoveTwoWayInts:
     arcpy.AddMessage("Removing Two Way Intersections")
