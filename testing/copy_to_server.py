3b6dc5b63a114b6e1b51b1dd9265e20f859a51c5
djconly85 <dconly@sacog.org>
2025-03-03 15:36:24 -0800

    change all tbx regpgm gp tasks to take free string for ptype

---
+++ testing/copy_to_server.py
@@ -0,0 +1,20 @@
+"""
+Name: copy_to_server.py
+Purpose: 
+
+
+Author: Darren Conly
+Last Updated: 
+Updated by: 
+Copyright:   (c) SACOG
+Python Version: 3.x
+"""
+from pathlib import Path
+import shutil
+
+
+
+
+
+if __name__ == '__main__':
+    
