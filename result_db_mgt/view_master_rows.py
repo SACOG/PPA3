3359c2244e905ea4a2c104a3a6787cb2e674cab9
djconly85 <dconly@sacog.org>
2025-03-27 11:17:21 -0700

    small tweaks

--- result_db_mgt/view_master_rows.py
+++ result_db_mgt/view_master_rows.py
@@ -7,8 +7,8 @@ from arcgis.features import GeoAccessor, GeoSeriesAccessor
 in_fc = r'\\Arcserverppa-svr\PPA_SVR\PPA_03_01\PPA3_GIS_SVR\PPA3_run_data.gdb\project_master'
 
 df = pd.DataFrame.spatial.from_featureclass(in_fc)
-cols = ['OBJECTID', 'proj_name', 'time_created',
-       'user_email', 'perf_outcomes']
+cols = ['proj_name', 'time_created',
+       'user_email']
 
 df = df.sort_values(by='time_created', ascending=False)
 
